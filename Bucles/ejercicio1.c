25e9aaa9568e112fd5d3585ec17fa5aa6545888d
JRDeveloper <jrdeveloper75@gmail.com>
2026-08-11 15:18:11 -0500

    Uso básico del bucle while

---
+++ Bucles/ejercicio1.c
@@ -0,0 +1,13 @@
+//Mostrar numeros del 1 al 10 usando un bucle while
+#include <stdio.h>
+
+int main (){
+    int contador = 1;
+
+    while (contador <= 10){
+        printf("%d\n", contador);
+        contador++; 
+    }
+
+    return 0;
+}
